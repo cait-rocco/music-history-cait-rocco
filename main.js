72af9cf5354b9a26d902b0429bd661b99dfe92c9
Cait Rocco <cait.rocco@gmail.com>
2017-06-30 11:03:37 -0500

    completed music history 3 exercise and some of 4

--- main.js
+++ main.js
@@ -1,22 +1,82 @@
-var songs = [];
+window.addEventListener("load", retrieveSongs);
 
-songs[songs.length] = "Stairway to Heaven > by Led Zeppelin on the album Led Zeppelin IV";
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-songs[songs.length] = "Creep > by Radiohead on the album Pablo Honey";
+let addSong = document.getElementById("addSong")
+let addArtist = document.getElementById("addArtist")
+let addAlbum = document.getElementById("addAlbum")
+let addBtn = document.getElementById("addBtn")
+let songList = document.getElementById("songTag");
+let songsArr = []
 
-for (var i = 0; i < songs.length; i++) {
-	songs[i] = songs[i].replace(/@|\(|!/, "")
-	songs[i] = songs[i].replace("*", "")
-	songs[i] = songs[i].replace(">", '-');
-	songs[i] = songs[i].split(/ - by | on the album /);
+function retrieveSongs () {
+		let songRequest = new XMLHttpRequest();
+		songRequest.addEventListener("load", songRequestLoad);
+		songRequest.open("GET", "songs.json");
+		songRequest.send();
+	};
+
+function songRequestLoad() {
+	var songsParse = JSON.parse(event.target.responseText).songs;
+	arrayUpSongs(songsParse);
 }
 
-var songTag = document.getElementById("songTag")
+// for (var i = 0; i < songs.length; i++) {
+// 	songs[i] = songs[i].replace(/@|\(|!/, "")
+// 	songs[i] = songs[i].replace("*", "")
+// }
 
-for (var i = 0; i < songs.length; i++) {
-	songTag.innerHTML += songs[i][0] + " by " + songs[i][1] + " on the album " + songs[i][2] + "<br>";
+function arrayUpSongs (songsParse) {
+		songsParse.forEach(function(song) {
+			let songObj = {"title": song.title, "artist": song.artist, "album": song.album}
+			songsArr.push(songObj);
+			songList.innerHTML += `${song.title} by ${song.artist} on the album ${song.album} <button id="dltBtn">Delete</button> <br>`
+		});
+		console.log("songObj", songsArr);
+		document.getElementById("dltBtn").addEventListener("click", function(){
+		songList.innerHTML = ""
+		});
+	}
+
+var viewLink = document.getElementById("viewLink")
+var addLink = document.getElementById("addLink")
+var listMusicView = document.getElementById("listMusicView")
+var addMusicView = document.getElementById("addMusicView")
+window.addEventListener("load", addMusicView.style.display = 'none')
+function listToggle() {
+	if (listMusicView.style.display === 'none') {
+        listMusicView.style.display = 'flex';
+        addMusicView.style.display = 'none'
+    } else {
+        listMusicView.style.display = 'none';
+    }
+ 	// listMusicView.classList.toggle('hidden')
 }
+
+function addToggle() {
+	// addMusicView.classList.toggle('hidden')
+	// listMusicView.classList.toggle('hidden')
+	if (addMusicView.style.display === 'none') {
+        addMusicView.style.display = 'block';
+        listMusicView.style.display = 'none'
+    } else {
+        addMusicView.style.display = 'none';
+    }
+}
+
+function addToArray() {
+	let userNewSong = {"title": addSong.value, "artist": addArtist.value, "album": addAlbum.value}
+	songsArr.push(userNewSong);
+	songList.innerHTML += `${addSong.value} by ${addArtist.value} on the album ${addAlbum.value} <button id="dltBtn">Delete</button> <br>`
+		console.log(songsArr);
+};
+
+viewLink.addEventListener("click", function(){
+	listToggle();
+})
+
+addLink.addEventListener("click", function() {
+	addToggle();
+})
+
+addBtn.addEventListener("click", function(){
+	addToArray();
+});
